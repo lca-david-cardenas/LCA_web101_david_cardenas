cc53f050372bd7cd9dddcbb307882a224e285622
lca.david.cardenas <lca.david.cardenas@outlook.com>
2020-03-15 17:33:09 -0500

    Adding towers of hanoi files

---
+++ MyPortfolio/js/towers_hanoi.js
@@ -0,0 +1,15 @@
+// FYI, this is built with jQuery
+'use strict';
+
+$(document).ready(function() {
+  let $block = null;
+
+  $('[data-row]').click(function() {
+    if ($block) { 
+      $(this).prepend($block);
+      $block = null;
+    } else {
+      $block = $(this).children().first().detach();
+    }
+  })
+})
