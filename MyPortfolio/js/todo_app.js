1aeff014c573cb597c916d82f418c388bc4a4e68
lca.david.cardenas <lca.david.cardenas@outlook.com>
2020-03-15 17:25:01 -0500

    Adding geolocation to dashboard

--- MyPortfolio/js/todo_app.js
+++ MyPortfolio/js/todo_app.js
@@ -1,3 +1,6 @@
+
+
+
 $(function() {
 
     let $list = $('ul');
@@ -23,3 +26,35 @@ $(function() {
     });
   
   });
+
+
+// You job is to comment the code here. 
+// Walk through step by step and write down what you think the code is doing
+// in the same format you're see this line of comments.
+
+var myLocation = document.getElementById("my-coordinates")
+
+function getLocation() {
+  if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(displayPosition)
+  } else {
+    document.getElementById("my-coordinates").innerHTML = "Not available"
+  }
+}
+
+/*
+function getLocation() {
+  if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(displayPosition)
+  } else {
+    document.getElementById("my-coordinates").innerHTML = "test";
+  }
+  
+}
+*/
+
+function displayPosition(position) {
+
+
+  document.getElementById("my-coordinates").innerHTML = "Longitude: " + position.coords.longitude.toFixed(4) + "<br><br> Latitude: " + position.coords.latitude.toFixed(4)
+}
